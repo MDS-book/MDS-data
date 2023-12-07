fcd18e87e16fe612a3556adc68c695887505e0c1
Stefan Sandfeld <s.sandfeld@fz-juelich.de>
2023-12-07 17:35:56 +0100

    add files

--- MDSdata/MDS2_Ising_model_light/dataset.py
+++ MDSdata/MDS2_Ising_model_light/dataset.py
@@ -8,11 +8,13 @@ from MDSdata.io import get_images_temperatures_and_labels
 
 """
 Two files are required: a zip archiv that contains a number of
-images without any directory, and a csv file that contains the three
+images without any directory, and a csv file that contains three
 columns with the names (as first row):
 filenames,temperatures,labels
 
-The labels tell if the temperature is below (0) or above the Curie temperature (1)
+The filenames must correspond to the the names in the zip archive.
+The labels (0 or 1) indicate if the temperature is below (0) or 
+above the Curie temperature (1).
 """
 
 # The absolute path is required when importing this package! Otherwise
@@ -24,16 +26,25 @@ p2 = join(os.path.dirname(os.path.abspath(__file__)), 'labels_16.csv')
 
 
 
+def data():
+    images, temperatures, labels = \
+        get_images_temperatures_and_labels(
+            zip_filename=p, 
+            csv_filename=p2
+        )
+    return images, temperatures, labels
+
 
 def main():
-    images, temperatures, labels = get_images_temperatures_and_labels(
-        zip_filename=p, 
-        csv_filename=p2
-    )
+    images, temperatures, labels = data()
 
-
-
-    plt.imshow(images[1])
+    fig, axes = plt.subplots(ncols=2, nrows=2, figsize=(8, 7),
+                             gridspec_kw={'hspace': 0.4, 'wspace': 0.3})
+    ax = axes.ravel()
+    
+    for i, idx in enumerate([10, 1500, 3000, 4500]):
+        ax[i].imshow(images[idx])
+        ax[i].set(title=f"T={temperatures[idx]:.2f},  label={labels[idx]}")
     plt.show()
 
 
